fdc98b1d288c1e3ba6a458a270529377285417dd
Carlos Alberto <carlos@spcbrasil.org.br>
2013-07-02 23:16:44 -0300

    compilou por carlos fatorial

---
+++ Fatorial.java
@@ -0,0 +1,14 @@
+public class Fatorial { 
+
+       public static void main (String arg[]){
+
+             System.out.println(fatorial());
+
+       }
+
+       public static int fatorial(){
+
+         return 5;
+       }
+
+}
