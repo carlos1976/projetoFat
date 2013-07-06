fd3d66309cbed4c022bc85db4cb42e9bcc11bf59
Carlos Alberto <carlos@spcbrasil.org.br>
2013-07-06 10:54:05 -0300

    alterei..

--- Fatorial.java
+++ Fatorial.java
@@ -2,13 +2,14 @@ public class Fatorial {
 
        public static void main (String arg[]){
 
-             System.out.println(fatorial());
+             System.out.println(fatorial()+"");
 
        }
 
        public static int fatorial(){
+     
+         return 7;
 
-         return 5;
        }
 
 }
